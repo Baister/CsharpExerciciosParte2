b5b59e22f677622990e872d3bc3a915a2fd7fc50
Baister <gustavobaister@gmail.com>
2025-01-28 13:48:47 -0300

    Exercicio Substituicao de Caractere

--- SubstituicaoDeCaractere/Program.cs
+++ SubstituicaoDeCaractere/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Substituição de Caracteres em Strings
+Peça uma frase ao usuário e um caractere para substituir. Depois, peça um caractere de substituição e exiba a nova frase. */
+
+using System.Globalization;
+
+Console.WriteLine("\n SUBSTITUIDOR DE CARACTERES 3000");
+Console.WriteLine("----------------------------------\n");
+
+Console.Write("Digite uma frase: ");
+var frase = Console.ReadLine();
+Console.Write("\nÓtimo, agora escolha um caractere que deseja substituir nessa frase: ");
+var caracter = Console.ReadLine();
+Console.Write("\nCerto, deseja substituir por qual letra: ");
+var caracterTrocado = Console.ReadLine();
+
+for (int i = 0; i < frase.Length; i++)
+{
+    if (caracter.Contains(frase[i]))
+    {
+        frase = frase.Replace(caracter, caracterTrocado);
+    }
+}
+Console.WriteLine(frase);
