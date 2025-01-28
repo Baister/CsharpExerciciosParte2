40ca2fde2a5d2b7540a7365b3939b26c56bd534b
Baister <gustavobaister@gmail.com>
2025-01-28 18:51:06 -0300

    Exercício de Contagem de Caracteres

---
+++ ContagemDeCaracteres/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Contagem de Caracteres Únicos
+Peça uma palavra e exiba quantos caracteres únicos ela contém. Exemplo: "banana" tem 3 caracteres únicos: b, a, n. */
+
+Console.WriteLine("\nCONTADOR DE CARACTERES ÚNICOS 3000");
+Console.WriteLine("------------------------------------\n");
+
+Console.Write("Digite uma palavra: ");
+var palavra = Console.ReadLine().ToLower();
+
+var caracteres = "";
+//var alfabetoComVogais = "abcdefghijklmnopqrstuvwxyz"; //Em vez de alfabetoComVogais usar char.IsLetter
+var contagem = 0;
+
+for (int i = 0; i < palavra.Length; i++)
+{
+    //Se tem todas as letras do alfabeto
+    if (char.IsLetter(palavra[i]))
+    {   //Se NÃO tem o caracter que já foi marcado passará por esse IF
+        if (!caracteres.Contains(palavra[i]))
+        {   //Concatenando
+            caracteres += Convert.ToString(palavra[i]);
+            //Console.WriteLine(caracteres);//teste
+
+            //Somando a contagem
+            contagem++;
+
+        }
+    }
+}
+Console.WriteLine($"'{palavra}' tem {contagem} caracteres únicos: {caracteres}.");
